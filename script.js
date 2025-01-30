fd6b7ac2797b82552f200abaf349088bc0b3b487
Elizabeth <elizabethg2796@tuckahoeschools.org>
2025-01-30 13:31:44 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/*if click stats()
+stats card appears 
+
+if click funfacts
+3 funfacts appear 
+
+index that can be entered into (f string + all lowercase)
+guess button to submit answer
+*/ 
+
+
